c0f2a176a0ea311484c5ee29e6915c007b795587
Justin Sanders <jsand155@ucr.edu>
2021-05-10 15:48:27 -0700

    Corrected Test output

--- tests/character_tests.hpp
+++ tests/character_tests.hpp
@@ -7,37 +7,42 @@
 
 TEST(Character_Getter_Tests, Default_Damage){
     Character* test = new Character();
-    EXPECT_EQ(test->getDmg(),"0");}
+    EXPECT_EQ(test->getDmg(),0);}
 
 TEST(Character_Getter_Tests, Default_Current_Health){
     Character* test = new Character();
-    EXPECT_EQ(test->getCurHP(),"0");}
+    EXPECT_EQ(test->getCurHP(),0);}
 
 TEST(Character_Getter_Tests, Default_Max_Health){
     Character* test = new Character();
-    EXPECT_EQ(test->getMaxHP(),"0");}
+    EXPECT_EQ(test->getMaxHP(),0);}
 
 TEST(Character_Getter_Tests, Default_Level){
     Character* test = new Character();
-    EXPECT_EQ(test->getLevel(),"0");}
+    EXPECT_EQ(test->getLevel(),0);}
 
 TEST(Character_Getter_Tests, Default_Experience){
     Character* test = new Character();
-    EXPECT_EQ(test->getExp(),"0");}
+    EXPECT_EQ(test->getExp(),0);}
 
 TEST(Character_Getter_Tests, Input_Damage){
     Character* test = new Character(10,100,1);
-    EXPECT_EQ(test->getDmg(),"10");}
+    EXPECT_EQ(test->getDmg(),10);}
+
 TEST(Character_Getter_Tests, Input_Current_Health){
     Character* test = new Character(10,100,1);
-    EXPECT_EQ(test->getCurHP(),"100");}
+    EXPECT_EQ(test->getCurHP(),100);}
+
 TEST(Character_Getter_Tests, Input_Max_Health){
     Character* test = new Character(10,100,1);
-    EXPECT_EQ(test->getMaxHP(),"100");}
+    EXPECT_EQ(test->getMaxHP(),100);}
+
 TEST(Character_Getter_Tests, Input_Level){
     Character* test = new Character(10,100,1);
-    EXPECT_EQ(test->getLevel(),"1");}
+    EXPECT_EQ(test->getLevel(),1);}
+
 TEST(Character_Getter_Tests, Input_Experience){
     Character* test = new Character(10,100,1);
-    EXPECT_EQ(test->getExp(),"0");}
+    EXPECT_EQ(test->getExp(),0);}
+
 #endif
