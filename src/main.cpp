e92e911390dacdfdb9d7c858045dd14d3004f927
Homestuck126 <overdiisc@gmail.com>
2021-05-11 14:15:37 -0700

    added sample main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 #include <iostream>
 int main()
 {
+Tile* beginning = new Tile ("sup I am a cool thing", 1);
+std::string sup = beginning->getdescription();
+std::cout<<sup<<std::endl;
 std::cout<<"HELLO WORLD"<<std::endl;	
 }
