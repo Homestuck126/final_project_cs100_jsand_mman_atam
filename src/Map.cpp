c2f34adfbd477e81e4fc90d2a47fa598e94583cc
Homestuck126 <overdiisc@gmail.com>
2021-05-13 12:22:44 -0700

    Fixed Map move implementation

--- src/Map.cpp
+++ src/Map.cpp
@@ -7,6 +7,7 @@ current = input;
 }
 Map::~Map()
 {
+//delete everything
 int size = list.size();
 for(unsigned i=0; i< size; i++)
 {
@@ -15,7 +16,10 @@ for(unsigned i=0; i< size; i++)
 }
 bool Map::move(char input)
 {
+bool move = false;
+//bool flag to check if moved
 	int in = 0; 
+//inout validation
 	if(input == 'l' || input == 'L')
 	in =1;
         if(input == 'r' || input == 'R')
@@ -25,16 +29,33 @@ bool Map::move(char input)
         if(input == 't' || input == 'T')
 	in =4;
 
+//movement
 	if(in==1 && current->getNext(1) !=nullptr)
+	{
+	move = true;
 	current= current->getNext(1);
+	}
         if(in==2 && current->getNext(2) !=nullptr)
+        {
+        move = true;
         current= current->getNext(2);
+        }
         if(in==3 && current->getNext(3) !=nullptr)
+        {
+        move = true;
         current= current->getNext(3);
+        }
+
         if(in==4 && current->getNext(4) !=nullptr)
+	{
+        move = true;
         current= current->getNext(4);
-	if (in ==0)
+        }
+	//if didn't move 
+	if (move == false)
 	std::cout<<"invalid input"<<std::endl;
+	//return
+	return move;
 
 }
 void Map::setLoc(Tile* start)
