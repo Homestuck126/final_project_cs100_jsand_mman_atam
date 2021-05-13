0fabf47228485760bdd8afd7b87485828031de13
Homestuck126 <overdiisc@gmail.com>
2021-05-13 12:28:35 -0700

    added main.cpp map construction and for loop for movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include "../header/Tile.hpp"
+#include "../header/Map.hpp"
 #include <iostream>
-int main()
+
+Map* generatemap()
 {
 Tile* beginning = new Tile ("sup I am a cool thing", 1);
-std::string sup = beginning->getdescription();
-std::cout<<sup<<std::endl;
+Tile* top = new Tile ("Sup I am top" ,1);
+Tile* right = new Tile ("sup I am right", 1);
+Tile* bot = new Tile ("Sup I am bot" ,1);
+Tile* left = new Tile ("Sup I am left" ,1);
+Map* test = new Map(beginning);
+test->insertTile(beginning);
+test->insertTile(top);
+test->insertTile(bot);
+test->insertTile(left);
+test->insertTile(right);
+test->setLoc(beginning);
+beginning->setAdj(left, right, top, bot);
+left->setAdj(nullptr, beginning, nullptr ,nullptr);
+right->setAdj(beginning, nullptr, nullptr ,nullptr);
+top->setAdj(nullptr,  nullptr ,nullptr, beginning);
+bot->setAdj(nullptr, nullptr,beginning ,nullptr);
+return test;
+}
+
+int main()
+{
+Map* map = generatemap();
+std::cout<<map->getCurrent()->getdescription();
+
+char input;
+while(input != 'q')
+{
+std::cin>>input;
+map->move(input);
+std::cout<<map->getCurrent()->getdescription();
+
+}
 std::cout<<"HELLO WORLD"<<std::endl;	
 }
