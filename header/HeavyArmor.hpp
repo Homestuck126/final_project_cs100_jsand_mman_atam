02ccedfd173c59f767824fed56af6d2119bc66f9
melineh025 <mmans025@ucr.edu>
2021-05-14 03:35:30 -0700

    Added concrete product classes that implement Armor

---
+++ header/HeavyArmor.hpp
@@ -0,0 +1,16 @@
+#ifndef __HEAVYARMOR_HPP__
+#define __HEAVY_ARMOR_HPP__
+
+#include "Armor.hpp"
+
+class HeavyArmor : public Armor {
+
+        public:
+                std::string getName() {return "HeavyArmor"};
+
+}
+
+
+
+#endif
+
