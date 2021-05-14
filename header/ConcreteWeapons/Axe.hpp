2dfe28b93ce17563be4cea145038fd5728ada28f
melineh025 <mmans025@ucr.edu>
2021-05-14 03:53:16 -0700

    Removed concrete classes from Abstract Weapon Product

---
+++ header/ConcreteWeapons/Axe.hpp
@@ -0,0 +1,18 @@
+#ifndef __AXE_HPP__
+#define __AXE_HPP__
+
+#include "../header/Weapon.hpp"
+
+class Axe : public Weapon {
+
+        public:
+                std::string getName() {
+
+                        return "Axe";
+                }
+
+
+
+}
+
+#endif
