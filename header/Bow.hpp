7ae2a56c6fc534b3cfaf98110d84d6a26314217a
melineh025 <mmans025@ucr.edu>
2021-05-14 03:35:00 -0700

    Added axe and bow concrete product classes

---
+++ header/Bow.hpp
@@ -0,0 +1,22 @@
+#ifndef __BOW_HPP__
+#define __BOW_HPP_
+
+#include "../header/GearFactory.hpp"
+
+class Bow : public Weapon {
+	
+	public:
+		std::string getName() {
+			return "Bow";
+		 	
+		}
+
+
+}
+
+
+
+
+
+
+#endif
