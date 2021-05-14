18e64a12dd60ad76a3d1dd365e14802bc65c71fa
Homestuck126 <overdiisc@gmail.com>
2021-05-13 21:13:42 -0700

    added CheckQuest and updateQuests test

--- tests/QuestLog_test.hpp
+++ tests/QuestLog_test.hpp
@@ -16,5 +16,36 @@ Questlog->addQuest(Quests);
         delete test2;
         delete Questlog;
 }
+TEST(QuestLogTest, QuestLogcheckQuest) {
+        Tile* test1 = new Tile();
+        Tile* test2 = new Tile();
+    Quest* Quests = new Quest("hi1" , "hi2" , "hi3" ,test1, test2 );
+        QuestLog* Questlog = new QuestLog();
+Questlog->addQuest(Quests);
+    EXPECT_EQ(Questlog->checkQuest(), "hi2" );
+        delete test1;
+        delete test2;
+        delete Questlog;
+}
+
+TEST(QuestLogTest, QuestLogupdateQuest) {
+        Tile* test1 = new Tile();
+        Tile* test2 = new Tile();
+    Quest* Quests1 = new Quest("hi1" , "hi2" , "hi3" ,test1, test2 );
+    Quest* Quests2 = new Quest("hi1" , "hi2" , "hi3" ,test1, test2 );
+	
+        QuestLog* Questlog = new QuestLog();
+Questlog->addQuest(Quests1);
+Questlog->addQuest(Quests2);
+Questlog->updateQuest();
+    EXPECT_EQ(Questlog->getCurrent(), Quests2 );
+        delete test1;
+        delete test2;
+        delete Questlog;
+}
+
+
+
+
 
 #endif //QUESTLOG_HPP
