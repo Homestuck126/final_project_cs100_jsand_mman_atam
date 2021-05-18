9a5c393881081a0f58c36e8dddc9871ab73e0038
melineh025 <mmans025@ucr.edu>
2021-05-18 15:10:29 -0700

    Fixed extra folders

--- header/AbstractFactory/ElfFactory.hpp
+++ header/AbstractFactory/ElfFactory.hpp
@@ -2,8 +2,8 @@
 #define __ELFFACTORY_HPP__
 
 
-//#include "GearFactory.hpp"
-#include "/home/csmajs/mmans025/final-project-jsand155_atam032_mmans025/header/GearFactory.hpp"
+#include "../GearFactory.hpp"
+//#include "/home/csmajs/mmans025/final-project-jsand155_atam032_mmans025/header/GearFactory.hpp"
 
 #include "/home/csmajs/mmans025/final-project-jsand155_atam032_mmans025/header/ConcreteWeapons/Bow.hpp"
 
