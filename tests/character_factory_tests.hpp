14e3b12e59b59ea3de5ee33a9c8f9a2de56187af
RiotRaider <jsand155@ucr.edu>
2021-05-21 18:57:51 -0700

    Added unit tests for second enemy type

--- tests/character_factory_tests.hpp
+++ tests/character_factory_tests.hpp
@@ -47,6 +47,48 @@ TEST(Generate_Enemy_Tests, Generate_Race1_Stat6){
         EXPECT_EQ(test->getLevel(), 0);
 	delete test;
 	delete fact;}
+TEST(Generate_Enemy_Tests, Generate_Race2_Stat1){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getDamage(),22);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race2_Stat2){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getProtection(),15);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race2_Stat3){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getMaxHP(), 80);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race2_Stat4){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getCurHP(), 80);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race2_Stat5){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getExperience(), 10);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race2_Stat6){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getLevel(), 0);
+        delete test;
+        delete fact;}
+
 
 
 
