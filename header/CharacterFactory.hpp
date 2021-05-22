a602f77f566a5d30544c025ae9ee4385f4f170a9
RiotRaider <jsand155@ucr.edu>
2021-05-21 18:51:41 -0700

    Added unit tests to confirm all stats of first race enemy

--- header/CharacterFactory.hpp
+++ header/CharacterFactory.hpp
@@ -36,3 +36,6 @@ class CharacterFactory {
 			}
 			
 };
+
+
+#endif
--- tests/character_factory_tests.hpp
+++ tests/character_factory_tests.hpp
@@ -6,15 +6,45 @@
 
 #include "../header/CharacterFactory.hpp"
 
-TEST(Generate_Enemy_Tests, Generate_Race1){
+TEST(Generate_Enemy_Tests, Generate_Race1_Stat1){
 	CharacterFactory* fact=new CharacterFactory();
-	Character* test = fact.getEnemy(0);
-	EXPECT_EQ(test->getDamage(),20);
-	EXPECT_EQ(test->getProtection(),10);
-	EXPECT_EQ(test->getMaxHP(), 100);
-	EXPECT_EQ(test->getCurHP(), 100);
-	EXPECT_EQ(test->getExperience(), 10);
-	EXPECT_EQ(test->getLevel(), 0);
+	Character* test = fact->getEnemy(0);
+	EXPECT_EQ(test->getDamage(),20);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race1_Stat2){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(0);
+	EXPECT_EQ(test->getProtection(),10);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race1_Stat3){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(0);
+        EXPECT_EQ(test->getMaxHP(), 100);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race1_Stat4){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(0);
+        EXPECT_EQ(test->getCurHP(), 100);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race1_Stat5){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(0);
+        EXPECT_EQ(test->getExperience(), 10);;
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race1_Stat6){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(0);
+        EXPECT_EQ(test->getLevel(), 0);
 	delete test;
 	delete fact;}
 
