2f1552555558f801b1c6fe69181204144336ec11
RiotRaider <jsand155@ucr.edu>
2021-05-27 21:19:11 -0700

    Changed to match new Character methods

--- header/CharacterFactory.hpp
+++ header/CharacterFactory.hpp
@@ -24,12 +24,12 @@ class CharacterFactory {
 				case 0:
 					wpn = ElfFact->createWeapon();
 					arm = ElfFact->createArmor();
-					enemy = new Character(wpn, arm, 10, 100, 0);
+					enemy = new Character("Default Elf",wpn, arm, 10, 100, 30);
 					break;	
 				case 1:
                                         wpn = OrcFact->createWeapon();
                                         arm = OrcFact->createArmor();
-                                        enemy = new Character(wpn, arm, 15, 80, 0);
+                                        enemy = new Character("Default Orc",wpn, arm, 15, 80, 30);
                                         break;
 				}
 			return enemy;
--- tests/character_factory_tests.hpp
+++ tests/character_factory_tests.hpp
@@ -9,72 +9,87 @@
 TEST(Generate_Enemy_Tests, Generate_Race1_Stat1){
 	CharacterFactory* fact=new CharacterFactory();
 	Character* test = fact->getEnemy(0);
-	EXPECT_EQ(test->getDamage(),20);;
+	EXPECT_EQ(test->getDamage(),20);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race1_Stat2){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(0);
-	EXPECT_EQ(test->getProtection(),10);;
+	EXPECT_EQ(test->getProtection(),10);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race1_Stat3){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(0);
-        EXPECT_EQ(test->getMaxHP(), 100);;
+        EXPECT_EQ(test->getMaxHP(), 100);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race1_Stat4){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(0);
-        EXPECT_EQ(test->getCurHP(), 100);;
+        EXPECT_EQ(test->getCurHP(), 100);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race1_Stat5){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(0);
-        EXPECT_EQ(test->getExperience(), 10);;
+        EXPECT_EQ(test->getExperience(), 30);
+        delete test;
+        delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race1_Name){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(0);
+        EXPECT_EQ(test->getName(), "Default Elf");
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race2_Stat1){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(1);
-        EXPECT_EQ(test->getDamage(),22);;
+        EXPECT_EQ(test->getDamage(),22);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race2_Stat2){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(1);
-        EXPECT_EQ(test->getProtection(),15);;
+        EXPECT_EQ(test->getProtection(),15);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race2_Stat3){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(1);
-        EXPECT_EQ(test->getMaxHP(), 80);;
+        EXPECT_EQ(test->getMaxHP(), 80);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race2_Stat4){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(1);
-        EXPECT_EQ(test->getCurHP(), 80);;
+        EXPECT_EQ(test->getCurHP(), 80);
         delete test;
         delete fact;}
 
 TEST(Generate_Enemy_Tests, Generate_Race2_Stat5){
         CharacterFactory* fact=new CharacterFactory();
         Character* test = fact->getEnemy(1);
-        EXPECT_EQ(test->getExperience(), 10);;
+        EXPECT_EQ(test->getExperience(), 30);
         delete test;
         delete fact;}
+
+TEST(Generate_Enemy_Tests, Generate_Race2_Name){
+        CharacterFactory* fact=new CharacterFactory();
+        Character* test = fact->getEnemy(1);
+        EXPECT_EQ(test->getName(), "Default Orc");
+        delete test;
+        delete fact;}
+
 
 
 
