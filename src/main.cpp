dc96b0b9344799aa97af6d294da81290cbddb38b
melineh025 <mmans025@ucr.edu>
2021-05-28 18:45:17 -0700

    Fixed input validation with numbers

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ std::cout<< "1 to move West, 2 to move east, 3 to move North, 4 to move South"<<
 std::cout<< "5 to check objective"<<std::endl;
 std::cin>>input;
 //determine user input
+
 if(input == 5)
 {
 std::cout<<quests->checkQuest()<<std::endl;
@@ -29,6 +30,12 @@ std::cout<<"SUP I AM NOT BROKEN"<<std::endl;
 }
 else if (input == 6)
 break;
+
+else if(input != 1 && input != 2 && input !=3 && input !=4 && input !=5 && input != 6) {
+std::cout << "Invalid input, uses numbers 1-6 on your keyboard to select" << std::endl;
+std::cin >> input;
+continue;
+}
 else
 map->move(input);
 std::cout<<"======================================================================================" <<std::endl;
