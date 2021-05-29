62ea5c3c1fa8b0dfb8eb1458b64e510522a1489a
melineh025 <mmans025@ucr.edu>
2021-05-29 15:17:11 -0700

    Testing composite pattern for Menu creation

--- header/Menu.hpp
+++ header/Menu.hpp
@@ -1,9 +1,11 @@
 #ifndef __MENU_HPP__
 #define __MENU_HPP__
 
-#include "../Map.hpp"
-#include "../QuestLog.hpp"
+//#include "../Map.hpp"
+//#include "../QuestLog.hpp"
 
+#include <string>
+#include "../header/Map/Map.hpp"
 
 class Menu {
 
@@ -11,6 +13,7 @@ class Menu {
 		virtual void print() = 0;
 		virtual char getChoice() = 0;
 		virtual bool getFlag() = 0;
+		virtual void setChoice(char) = 0;
 	protected:
 		char choice;
 		bool flag;
@@ -18,36 +21,79 @@ class Menu {
 };
 
 
+
+class MapMenu : public Menu {
+
+        private:
+        Map * newMap;
+
+        public:
+                MapMenu(Map* map) {this->newMap= map;}
+                void print() {
+                                std::cout << "1 - Move West\n2 - Move East\n3 - Move North\n4 - Move South" << std::endl;
+                                }
+                char getChoice() {
+                        return choice;
+                }
+                bool getFlag() {return newMap->move(choice);} //have to implement?
+
+                void setChoice() {std::cin >> choice;}
+
+};
+
+
+
+
+
+
 class CharMenu : public Menu {
+	
+	private:
+	MapMenu *mapM;
+//	Map *newMap;
 
 	public:
+		
 		void print() {std::cout << "1 - Move \n2 - Check Objective\n3 - Check Inventory\n4 - Quit Game" << std::endl;}
-		char getChoice () {return (std::cin >> choice;}
-		bool getFlag() { return false; }
+		
+		void setChoice() {
+				  std::cin >> choice;
+				  switch(choice) {
+					case '1':
+						mapM->print();
+						mapM->setChoice();
+						mapM->getFlag();				
+						break;
+					case '2':
+						break;
+					case '3':
+						break;
+					case '4':
+						flag = false;
+						break;
+					}
+
+				}
+		char getChoice () {return choice;}
+		bool getFlag() { if(quests->compareQuest(map->getCurrent())) {
+						return false;
+					} 
+				}
 
 
 };
 
 
 class CombatMenu : public Menu {
+		 void setChoice(char c) {choice = c;}
 
 		void print () {std::cout << "" << std::endl;}
-		char getChoice () {return std::cin >> choice;}
+		char getChoice () {return  choice;}
 		bool getFlag() {return false;}
 
 };
 
 
-class MapMenu : public Menu {
-	public:
-		void print() {std::cout << "1 - Move West\n2 - Move East\n3 - Move North\n4 - Move South" << std::endl};
-		char getChoice() {
-			return ( std::cin >> choice);
-		}
-		bool getFlag() {return false;} //have to implement?
-
-		
-};
 
 
 
