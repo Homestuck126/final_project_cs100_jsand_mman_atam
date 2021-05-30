b964e090e35466af59c47e7dac89509fe4a194bb
Homestuck126 <overdiisc@gmail.com>
2021-05-30 12:59:22 -0700

    Fix bug with segmentation fault when moving out of bounds

--- src/Map/Map.cpp
+++ src/Map/Map.cpp
@@ -26,12 +26,13 @@ bool move = false;
 //input validation
 char c = input;
 //movement
-	if(c=='1' && current->getNext(1) !=nullptr)
+	if(c=='1' && current->getNext('1') !=nullptr)
 	{
+		std::cout<<"I am not the problem map"<<std::endl;
 	move = true;
 	current= current->getNext('1');
 	}
-        if(c=='2' && current->getNext(2) !=nullptr)
+        if(c=='2' && current->getNext('2') !=nullptr)
         {
         move = true;
         current= current->getNext('2');
