dbb18924d4b7b6a021ceab521c4dd5194655fdeb
melineh025 <mmans025@ucr.edu>
2021-05-30 06:04:09 -0700

    initial combat class definition

---
+++ header/Combat.hpp
@@ -0,0 +1,99 @@
+#ifndef __COMBAT_HPP__
+#define __COMBAT_HPP__
+
+
+#include "../header/Character.hpp"
+#include "../header/CharacterFactory.hpp"
+//#include ""
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+
+class Combat {
+
+	private:
+		//CombatMenu *coMenu;
+		//
+		//
+
+		CharacterFactory *cF;
+
+		Character *enemy;
+		Player *player;
+
+
+		bool playerWon;
+		bool isDone;
+		bool missed;
+	public:
+		Combat(CharacterFactory *c) {
+			this->cF = c;
+			this->enemy = cF->getEnemy(0,0,1);
+			this->player = cF->getPlayer("mel",0);
+
+			isDone = false;
+			missed = false;
+			playerWon = false;
+		}
+		bool getDone() {return isDone;}
+		bool startCombat() {
+		
+			setMissed();
+			introLines();
+			playerLines();
+		
+			enemy->takeDamage(player->getDamage());
+			if(enemy->getCurHP() == 0) {
+				playerWon = true;
+				isDone = true;
+			}
+			else if(player->getCurHP() == 0) {
+				playerWon = false;
+				isDone = true;
+			}
+		
+			if(!missed) {
+				attackLines();
+				player->takeDamage(enemy->getDamage());
+			}
+		}
+		
+		
+		void setMissed() {
+		
+			
+			int randNum = std::rand() % 100 + 1; 
+			if(randNum > 60) {
+				missed = true;
+			}
+			else {
+				missed = false;
+			}
+
+		}
+		//dialogue combat options
+		void playerLines() {
+			std::cout << "You hit it with " << player->getDamage() << std::endl;
+
+		}
+		void introLines() {
+			std::cout << "A wild appeared" << std::endl;
+
+		}
+		void attackLines() {
+		std::cout << enemy->getName() << " hit you with " << enemy->getDamage() << "damage!" << std::endl;
+
+		}
+		
+
+
+};
+
+
+
+
+#endif
