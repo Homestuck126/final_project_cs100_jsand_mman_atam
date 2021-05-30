71ebe6624c84284e3f18484de74b01dd383eb5ec
Aaron Tam <atam032@ucr.edu>
2021-05-30 15:57:35 -0700

    Update InitialPlayer.hpp

--- header/Intitialization/InitialPlayer.hpp
+++ header/Intitialization/InitialPlayer.hpp
@@ -25,9 +25,9 @@ public:
         start:
         std::cout<<"What is your Name" <<std::endl;
         std::cin>> name;
-        std::cout<<"Enter who you were before the recruitement\n1 - You were a homeless hobo, your posessions numbered the old gun your pal gave you before he passed and the tattered rags you wear. You can cause some havoc with the gun you own, but, your unhealthy life before and tattered armor does not make you very tough."<<std::endl;
+        std::cout<<"Enter who you were before the recruitement\n1 - You were a homeless hobo, your possessions numbered the old gun your pal gave you before he passed and the tattered rags you wear. You can cause some havoc with the gun you own, but, your unhealthy life before and tattered armor does not make you very tough."<<std::endl;
         std::cout<<"2 - You were a space ranger, you were disgraced from your position after crashing your ship on a planet. Now all you've got is your trusty las rifle, and the reflective jacket you wore. You're a pretty balanced character."<<std::endl; 
-        std::cout<<"3 - You were a Riot officer on one of the core worlds, you've been reassigned by your commander to the planetary exploration Beaureau. A demotion for sure, but they allowed you to keep your armored suit and your shock baton. You might not be able to move very well or hit very hard, but your armor protects you against the strongest of hits." <<std::endl;
+        std::cout<<"3 - You were a Riot officer on one of the core worlds, you've been reassigned by your commander to the planetary exploration Bureau. A demotion for sure, but they allowed you to keep your armored suit and your shock baton. You might not be able to move very well or hit very hard, but your armor protects you against the strongest of hits." <<std::endl;
         std::cin>>input;
         switch(input)
          {
