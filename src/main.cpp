eafbe0f477a6dd5f360069d9715ebe7f07ff1bbb
RiotRaider <jsand155@ucr.edu>
2021-05-31 05:34:12 -0700

    Fixed multi-run error

--- src/main.cpp
+++ src/main.cpp
@@ -38,49 +38,75 @@ int main(){
         std::cout << "=========================================\n";
 	std::cout << "A game made by Aaron, Justin, and Melineh\n";
 //Start Menu Loop
-		//Create new Player
-		std::cout << "=========================================\n";
-		std::cout<<"**CHARACTER CREATION**\n";
-		player = pGen->createPlayer(CharCreator);
-		//Create Map and Populate Quests
-		worldGen = new Initialization(map, quests);
-		menu = new CoreMenu(map, quests, player);
-		while(!game_done){
-			while(menu->getFlag()){
-			std::cout << "=========================================\n";
-			menu->menu();
-			}
-			if(player->getCurHP()<=0){
-                		std::cout << player->getName() << " has been beaten. Their adventure is over. What a shame...\n" ;
-                	        game_done = 1;
-			}else if(quests->compareQuest(map->getCurrent())){
-				std::cout << player->getName() << " managed to follow orders and have a wonderful time. Good job!\n";
-				game_done = 1;
-			}else if(menu->getMoveFlag()){
-				if(rand()%5>=2){
-					battle = new Combat(CharCreator, player);
-					battle->startCombat();
-					delete battle;
+	while(choice!='2'){
+		std::cout << "***MAIN MENU***\n\nPlease make a selection\n1 - Start New Game\n2 - Quit\n";
+		std::cin >> choice;
+		switch(choice){
+			case '1':
+				//Reset Game Objects if not first play during run
+				if(player){
+				delete player;
+				delete quests;
+				delete map;
+				delete worldGen;
+				delete menu;
+				map = new Map();
+				quests = new QuestLog();
+				game_done = false;
 				}
-			}else{
-				std::cout << "Progress was not saved\n\n";
-				game_done = 1;
-			}
+				//Create new Player
+				std::cout << "=========================================\n";
+				std::cout<<"**CHARACTER CREATION**\n";
+				player = pGen->createPlayer(CharCreator);
+				//Create Map and Populate Quests
+				worldGen = new Initialization(map, quests);
+				menu = new CoreMenu(map, quests, player);
+				while(!game_done){
+					while(menu->getFlag()){
+					std::cout << "=========================================\n";
+					menu->menu();
+					}
+					if(player->getCurHP()<=0){
+                				std::cout << player->getName() << " has been beaten. Their adventure is over. What a shame...\n" ;
+                			        game_done = 1;
+					}else if(quests->compareQuest(map->getCurrent())){
+						std::cout << player->getName() << " managed to follow orders and have a wonderful time. Good job!\n";
+						game_done = 1;
+					}else if(menu->getMoveFlag()){
+						if(rand()%5>=2){
+							battle = new Combat(CharCreator, player);
+							battle->startCombat();
+							delete battle;
+						}
+					}else{
+						std::cout << "Progress was not saved\n\n";
+						game_done = 1;
+					}
+				}
+				break;
+			case '2':
+				std::cout <<"\n\nTHANK YOU FOR PLAYING!!!\n\n\n";
+				std::cout << "=========================================\n";
+				break; 
+			default: 
+				std::cout << "Invalid Selection\n\n\n";
+				break;
 		}
-
-	
+	}
 	std::cin.clear();
 	std::cin.ignore();	
 
 
 	//Delete any objects created during runtime
-	delete player;
-	delete quests;
-	delete map; 
-	delete worldGen;
-	delete menu;
-	delete pGen; 		//delete Player Initializer
-	delete CharCreator; 	//delete CharacterFactory
+	if(player){
+		delete player;
+		delete quests;
+		delete map; 
+		delete worldGen;
+		delete menu;
+		delete pGen; 		//delete Player Initializer
+		delete CharCreator; 	//delete CharacterFactory
+		}
 
 	return 0;
 }
